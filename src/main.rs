14d157f2d1ec1cbdc1375493bcdf4412715626af
Dave Beesley <david.beesley@pm.me>
2023-03-01 16:22:59 -0800

    chat

--- src/main.rs
+++ src/main.rs
@@ -44,36 +44,6 @@ async fn chat_with_ai() -> Result<(), GptError> {
     let model = ModelId::Gpt3Period5Turbo;
     let max_tokens = 50;
     let mut chat_history = ChatHistory::new();
-
-    // loop {
-    //     // Get input from user
-    //     let mut input = String::new();
-    //     println!("You: ");
-    //     io::stdin().read_line(&mut input)?;
-    //     let input = input.trim();
-
-    //     // If user wants to exit, break the loop
-    //     if input.eq_ignore_ascii_case("exit") {
-    //         break;
-    //     }
-
-    //     // Create chat message from user input and add it to chat history
-    //     let user_message = ChatMessage {
-    //         role: Role::User,
-    //         content: input.to_string(),
-    //     };
-    //     chat_history.add_message(user_message);
-
-    //     // Chat with AI
-    //     let ai_message = client
-    //         .chat(model, max_tokens, chat_history.get_history().to_vec())
-    //         .await?;
-
-    //     chat_history.add_message(ai_message.clone());
-
-    //     // Print AI response
-    //     println!("AI: {}", ai_message.content);
-    // }
     loop {
         // Get input from user
         let mut input = String::new();
